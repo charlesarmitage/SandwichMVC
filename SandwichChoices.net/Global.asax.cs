58a515932c9bb93a45d6abe2bc0c2db3172a47dd
Charles Armitage <code@cjrarmitage.co.uk>
2014-02-13 22:29:46 +0000

    Add Sandwich initializor to pre-populate db

--- SandwichChoices.net/Global.asax.cs
+++ SandwichChoices.net/Global.asax.cs
@@ -22,6 +22,8 @@ namespace SandwichChoices.net
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            System.Data.Entity.Database.SetInitializer(new SandwichInitializor());
         }
     }
 }
---
+++ SandwichChoices.net/SandwichInitializor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SandwichChoices.net.Models;
+
+namespace SandwichChoices.net
+{
+    public class SandwichInitializor : DropCreateDatabaseIfModelChanges<Models.SandwichDbContext>
+    {
+        protected override void Seed(Models.SandwichDbContext context)
+        {
+            context.Sandwiches.Add(new Sandwich { Name = "Cheese", Ingredients = "Cheese", FlavourRating = 9, Priority = 7});
+            context.Sandwiches.Add(new Sandwich { Name = "Cheese & Pickle", Ingredients = "Cheese, Pickle", FlavourRating = 10, Priority = 10});
+            context.Sandwiches.Add(new Sandwich { Name = "Cheese & Tomato", Ingredients = "Cheese, Tomato", FlavourRating = 8, Priority = 6});
+            base.Seed(context);
+        }
+    }
+}
